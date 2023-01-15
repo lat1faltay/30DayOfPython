686b0a8fa9b46f56af623961858b7e1f0159cc98
Latif Altay <latifaltay00@gmail.com>
2023-01-15 16:37:53 +0300

    Day1

---
+++ day1/day1.py
@@ -0,0 +1,43 @@
+print("Addition")
+print(2+3) # Addition (+)
+print("Subtraction")
+print(3-1) # Subtraction (-)
+print("multipilication")
+print(2*3) # Multiplication (*)
+print("Division")
+print (3/2) # Division(/)
+print("Exponential")
+print(3**2) # Exponential (**)
+print("Modulus")
+print(3%2) # Modulus (%)
+print("Floor Division Operator")
+print(3//2) # Floor division operator
+
+
+# Data Types
+print("Data Types")
+print("*"*25)
+
+print("Int")
+print(type(10)) # Int
+
+print("Float")
+print(type(3.14)) # Float
+
+print("Complex")
+print(type(1+3j)) # Complex
+
+print("String")
+print(type("Hello World!")) # String
+
+print("List")
+print(type([1, 2, 3])) # List
+
+print("Dictionary")
+print(type({'name':'Latif'})) # Dictionary
+
+print("Set")
+print(type({9.8, 3.14, 2.7})) # Set
+
+print("Tuple")
+print(type((9.8, 3.14, 2.7))) # Tuple
